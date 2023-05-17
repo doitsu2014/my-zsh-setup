cafbd2a1709ff12bc274dd67e781d94f6043b4e7
root <root@DESKTOP-IM1P9EL>
2023-05-17 22:55:32 +0700

    latest

--- utilities/setup-workspace-debian.sh
+++ utilities/setup-workspace-debian.sh
@@ -17,9 +17,19 @@ else
   sudo apt-get remove docker docker-engine docker.io containerd runc -y
   sudo apt-get update -y
   sudo apt-get install ca-certificates curl gnupg -y
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-  sudo usermod -aG docker $USER
+
+  sudo install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+ echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list -y > /dev/null
+
+  sudo apt-get update -y
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+  sudo usermod -aG docker $USER -y
 fi
 
 echo "Install microk8s using snap"
@@ -47,3 +57,6 @@ echo "Install helm"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+
+echo "Install nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
