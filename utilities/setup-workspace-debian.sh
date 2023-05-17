9dd1b0e801e8733cea8b7dd3d8fbeb4c65ab761d
root <root@DESKTOP-IM1P9EL>
2023-05-17 23:02:15 +0700

    latest

--- utilities/setup-workspace-debian.sh
+++ utilities/setup-workspace-debian.sh
@@ -60,3 +60,21 @@ chmod 700 get_helm.sh
 
 echo "Install nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+echo "Install terraform"
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common -y
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+sudo apt-get install terraform
+
